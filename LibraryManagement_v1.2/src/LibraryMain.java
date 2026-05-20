c9252602a4700c91ea3e6cb88bba6629e93c00e5
woohyun-park <qkrdngus1227@gmail.com>
2026-05-20 11:19:46 +0900

    github issues #1

--- LibraryManagement_v1.2/src/LibraryMain.java
+++ LibraryManagement_v1.2/src/LibraryMain.java
@@ -4,7 +4,7 @@ import java.util.*;
  * 도서 관리 시스템의 메인 클래스
  * <p>사용자 인터페이스(CLI)를 제공하며, DB 연결하여 권한에 따른 메뉴 출력 및 사용자 입력을 처리합니다.</p>
  * <p>2026년 5월 13일 시작</p>
- * 
+ *
  * @author Su Man Nam
  * @version 1.2
  */
@@ -152,7 +152,8 @@ public class LibraryMain {
     /**
      * 도서 정보의 수정 및 삭제를 처리하는 UI입니다.
      * <p>ID를 통해 도서를 조회하고, 선택에 따라 제목/저자 수정 또는 삭제를 수행합니다.</p>
-     *
+     * <p>수정과 삭제의 효율화를 위해 같은 기능으로 개발(26.05.20; 우현)</p>
+     * @see <a href="https://github.com/park-woohyun/LibraryManagement/issues/1">Issue #1: 한 책 삭제시 DB에서 해당 책이 삭제 안됨</a>
      * @see LibraryManager#deleteBook(int)
      */
     private static void editOrDeleteUI() {
@@ -190,6 +191,8 @@ public class LibraryMain {
                     book.setTitle(newTitle);
                     System.out.println("[결과] 제목이 수정되었습니다.");
                 }
+                // DB 저장
+                manager.saveChanges();
             }
             case 2 -> {
                 System.out.print("- 새 저자 입력: ");
@@ -198,17 +201,17 @@ public class LibraryMain {
                     book.setAuthor(newAuthor);
                     System.out.println("[결과] 저자명이 수정되었습니다.");
                 }
+                // DB 저장
+                manager.saveChanges();
             }
             case 3 -> {
                 manager.deleteBook(id);
+                // 삭제 쿼리 작성
                 System.out.println("[결과] 삭제되었습니다.");
 
 
             }
         }
-
-        // DB 저장
-        manager.saveChanges();
     }
 
     /**
